ff9698008fea7125846dc52f8e758acc8c1d2efc
magda-korzeniowska <m.korzeniowska@tlen.pl>
2019-05-09 14:01:34 +0200

    Add changes for DeckList - fetching decks

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { getDecks } from '../utils/api'
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux'
+import { receiveDecks } from '../actions'
+
 
 class DeckList extends Component {
 
+  componentDidMount() {
+    const { dispatch } = this.props
+
+    getDecks()
+      .then((decks) => dispatch(receiveDecks(decks)))
+  }
+
   render() {
+    const { decks } = this.props
 
     return (
       <View>
-        <Text>Deck List</Text>
+        {Object.keys(decks).map((deck) => {
+          const { title, question } = decks[deck]
+          return (
+            <View key={deck}>
+              <Text>{title}</Text>
+            </View>
+          )
+        })}
       </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -20,4 +40,10 @@ const styles = StyleSheet.create({
   }
 })
 
-export default DeckList
+function mapStateToProps(decks) {
+  return {
+    decks
+  }
+}
+
+export default connect(mapStateToProps)(DeckList)
