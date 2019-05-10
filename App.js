c0ddf93d942cd30e310a6f7e6b0fbc83cf599946
magda-korzeniowska <m.korzeniowska@tlen.pl>
2019-05-10 18:38:12 +0200

    Add StatusBar

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import MainNavigator from './components/MainNavigator' 
+import MainNavigator from './components/MainNavigator'
+import { Constants } from 'expo'
+import { yellow } from './utils/colors'
+
+function AppStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
+          <AppStatusBar backgroundColor={yellow} barStyle='light-content'/>
           <MainNavigator />
         </View>
       </Provider>
